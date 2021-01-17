a90938dafcd40987754a4189c4c1ab0b216468bc
Jay <73120876+JarrellW@users.noreply.github.com>
2021-01-17 12:27:38 -0500

    Update VideoController.java

--- src/main/java/com/Zipcode/Rocks/Controllers/VideoController.java
+++ src/main/java/com/Zipcode/Rocks/Controllers/VideoController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("storage/")
-@CrossOrigin(origins = {"https://ziptubefrontend.herokuapp.com", "http://localhost:8080", "http://localhost"})
+@CrossOrigin(origins = {"https://ziptubefrontend.herokuapp.com", "http://localhost:8080", "http://localhost:4200"})
 public class VideoController {
 
     @Autowired
